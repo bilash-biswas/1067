1b93923a82cef8a1c1aa5e71cb6d78a1b02c0dee
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-17 13:29:53 +0600

    Create Kotlin.kt

---
+++ Kotlin.kt
@@ -0,0 +1,11 @@
+import java.util.Scanner
+
+fun main(args: Array<String>){
+    val input = Scanner(System.`in`);
+    var a = input.nextInt();
+    for(i in 1..a step 2)
+    {
+        println(i)
+    }
+}
+
